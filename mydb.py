25d40971580e39e824b13189b4d1dfe5aa750e39
Skander Bennia <mohamedskander.bennia@gmail.com>
2023-07-16 16:34:10 +0200

    initial commit

---
+++ mydb.py
@@ -0,0 +1,25 @@
+# install Mysql on your computer 
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='',
+)
+
+
+#prepare a cursor object
+
+cursorObject = dataBase.cursor()
+
+
+# create a database
+
+cursorObject.execute("CREATE DATABASE IF NOT EXISTS elderco")
+
+print("All Done!")
